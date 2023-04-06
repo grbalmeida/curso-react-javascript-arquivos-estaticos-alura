658fc7a6b3f6b10c5f496559cab9907076006a7f
grbalmeida <g.r.almeida@live.com>
2023-04-06 19:09:53 -0300

    Criando o menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Menu from "./componentes/Menu";
 import PaginaInicial from "./paginas/PaginaInicial";
 import './styles/estilosGlobais.scss';
 
 export default function App() {
   return (
-    <PaginaInicial />
+    <>
+      <PaginaInicial />
+      <Menu />
+    </>
   );
 }
