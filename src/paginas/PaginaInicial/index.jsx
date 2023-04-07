6d9d328872f8dbccc09168ae765ea4faa03128f3
grbalmeida <g.r.almeida@live.com>
2023-04-07 18:48:14 -0300

    Seção de populares

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,8 +1,9 @@
 import Cabecalho from '../../componentes/Cabecalho';
 import Menu from '../../componentes/Menu';
 import Banner from '../../componentes/Banner';
-import Rodape from '../../componentes/Rodape';
 import Galeria from '../../componentes/Galeria';
+import Populares from '../../componentes/Populares';
+import Rodape from '../../componentes/Rodape';
 import styles from './PaginaInicial.module.scss';
 
 export default function PaginaInicial() {
@@ -14,10 +15,11 @@ export default function PaginaInicial() {
                     <Menu />
                     <Banner />
                 </section>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
             </main>
-            <div className={styles.galeria}>
-                <Galeria />
-            </div>
             <Rodape />
         </>
     );
