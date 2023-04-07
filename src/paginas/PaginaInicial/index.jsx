892351fa675675bd90c3557f702ca35949e6be85
grbalmeida <g.r.almeida@live.com>
2023-04-07 18:58:30 -0300

    Absolute Imports

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,10 @@
-import Cabecalho from '../../componentes/Cabecalho';
-import Menu from '../../componentes/Menu';
-import Banner from '../../componentes/Banner';
-import Galeria from '../../componentes/Galeria';
-import Populares from '../../componentes/Populares';
-import Rodape from '../../componentes/Rodape';
 import styles from './PaginaInicial.module.scss';
+import Cabecalho from 'componentes/Cabecalho';
+import Menu from 'componentes/Menu';
+import Banner from 'componentes/Banner';
+import Galeria from 'componentes/Galeria';
+import Populares from 'componentes/Populares';
+import Rodape from 'componentes/Rodape';
 
 export default function PaginaInicial() {
     return (
